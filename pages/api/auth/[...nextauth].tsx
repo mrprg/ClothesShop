192a474eb7ac251a8f662c5e051548f4796693d4
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-04-14 18:55:58 +0430

    Convert files to typeScript from auth folder

---
+++ pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,38 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      type: "credentials",
+      credentials: {},
+      authorize(credentials, req) {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+      
+        if (email !== "john@gmail.com" || password !== "1234") {
+          throw new Error("invalid credentials");
+        }
+
+        return {
+          id: "1234",
+          name: "John Doe",
+          email: "john@gmail.com",
+          role: "admin",
+        };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signin",
+    // error: '/auth/error',
+    // signOut: '/auth/signout'
+  },
+};
+
+export default NextAuth(authOptions);
