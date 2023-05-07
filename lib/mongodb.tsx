84309e4398d5c36f494781fe319e3c0909c5e23a
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-05-07 15:32:05 +0430

    FIX ERROR "TypeError: resolver is not a function"

---
+++ lib/mongodb.tsx
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+let client;
+let clientPromise;
+
+if (process.env.NODE_ENV === "development") {
+  let globalWithMongo = globalThis;
+  if (!globalWithMongo._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    globalWithMongo._mongoClientPromise = client.connect();
+  }
+  clientPromise = globalWithMongo._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
